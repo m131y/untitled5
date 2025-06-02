eb39f4eb562c6deb8a1d4fc715f8af6bc5f1bf4a
whals <whalsdud0131@gmail.cm>
2025-06-02 13:30:36 +0900

    else

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 5;
+        int number = -1;
 
-        if (number >= 5) {
+        if (number > 0) {
             System.out.println("number는 0보다 큽니다.");
+        } else {
+            System.out.println("number는 0보다 작습니다.");
         }
     }
 }
