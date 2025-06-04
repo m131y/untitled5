7aa8d8c353cd67b0ec88464b3db73434f30d2491
whals <whalsdud0131@gmail.cm>
2025-06-04 10:11:28 +0900

    && ||

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        String mood = "tired";
-        // "happy", "angry", "sad", etc.
+        int age = 20;
+        boolean hasID = true;
 
-        if (mood == "happy") {
-            System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다.");
-        } else if (mood == "angry") {
-            System.out.println("나는 화가난다. 왜냐하면 화가나기 때문이다.");
-        } else if (mood == "sad") {
-            System.out.println("나는 슬프다. 왜냐하면 슬프기 때문이다.");
+        if(age >= 18 && hasID) {
+            System.out.println("true");
+        }
+
+        boolean isWeekend = false;
+        boolean isHoliday = true;
+
+        boolean isRaining = true;
+
+        if(!isRaining) {
+            System.out.println();
         } else {
-            System.out.println("지금 내 기분은 이 단어들로는 표현할 수 없다.");
+            System.out.println();
         }
     }
 }
