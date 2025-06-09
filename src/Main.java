a93bae049c51c8d31adf043c560770f6e3696350
whals <whalsdud0131@gmail.cm>
2025-06-09 14:51:07 +0900

    Array

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, Java!";
+        int[] numbers = {2, 4, 6, 8, 10};
+        int sum = 0;
 
-        for (int i = 0; i < message.length(); i++) {
-            System.out.println(i);
-            if (message.charAt(i) == 'l') {
-                break;
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
         }
+        System.out.println(sum);
     }
 }
